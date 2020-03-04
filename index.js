2dc2d59742d11fbbbd2199bbe3b8e6ec10b090cf
monikamohan98gmailcom <monikamohan98@gmail.com>
2020-03-04 15:46:20 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*
+/*string methods
 two types of data- primitive datatype
                   -reference
   primitive-strings,number,boolean,sumbol
@@ -58,3 +58,63 @@ console.log(a.toLowerCase())
 //it removes the white spaces in the strings
 //output=this is a sentence
 console.log(b.trim())
+
+/*
+-----------------------------------------
+*/
+/*
+number methods
+*/
+//it changes the number to string values
+//output=1234.56789
+var number=1234.56789;
+console.log(number.toString())
+console.log(typeof(number.toString()))
+//it rounds the value and changes it to exponential value
+//output=1.23456789e+3
+console.log(number.toExponential())
+//it rounds off the value in the number
+//output=1234.57
+console.log(number.toFixed(20))
+
+/*
+----------------------------------------------------------
+*/
+/*
+global methods
+*/
+
+//console object hs different type of displays
+console.info('hii this is monika')
+console.log('hii this is monika')
+console.table(["Audi", "Volvo", "Ford"]);
+console.warn('hii this is monika')
+console.error('hii this is monika')
+
+//alert gives the pop up box
+//alert("you are using monika's webites")
+
+//prompt give the pop up with  the feed given whic is storedin variable
+//var userName=prompt("Enter your user name","monika")
+
+//confirm also displays pop up window
+//confirm('are you sure you want to exit')
+
+//Number definesthe number for the variable given
+//output=0
+console.log(Number(false));
+//returns boolean value if it is not a number 
+//output=false
+console.log(isNaN(123.45));
+//parses a string and returns a number. Spaces are allowed. Only the first number is returned
+//output=10.2334
+console.log(parseFloat('10.2334 years'))
+//parses a string and returns a whole number. Spaces are allowed. Only the first number is returned:
+//output=12
+console.log(parseInt('12.73*34'))
+//it returns the string values
+//output=false
+console.log(String(Boolean(0)))
+//boolean returns true or false
+//output=true
+console.log(Boolean('moni'))
